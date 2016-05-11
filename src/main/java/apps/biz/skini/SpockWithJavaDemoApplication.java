46987a975aa96433c503ad2f0098b92f52fb1a02
skinisbizapps <skini.biz.apps@icloud.com>
2016-05-10 22:49:59 -0700

    Initial commit

---
+++ src/main/java/apps/biz/skini/SpockWithJavaDemoApplication.java
@@ -0,0 +1,12 @@
+package apps.biz.skini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockWithJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockWithJavaDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/apps/biz/skini/SpockWithJavaDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package apps.biz.skini;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpockWithJavaDemoApplication.class)
+@WebAppConfiguration
+public class SpockWithJavaDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
